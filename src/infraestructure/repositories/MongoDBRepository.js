97d024f0f9bf11e986e60611e65a3f5ed313b2ed
rominasoveranocode <rominasoverano.trabajo@gmail.com>
2025-09-03 23:14:29 -0300

    add MongoDBRepository (CRUD)

--- src/infraestructure/repositories/MongoDBRepository.js
+++ src/infraestructure/repositories/MongoDBRepository.js
@@ -1,15 +1,33 @@
-import { connection } from "../database/MongoDBConnection.js";
-import { ProductModel } from "../database/models/ProductModel.js";
-
-class MongoDBManager{
-    createProduct = async (product) => {
+export class MongoDBRepository {
+    constructor(model){
+        this.model = model
+    }
+    create = async (product) => {
         try {
-            connection.initAtlasMongoDB()
-            return await ProductModel.create(product);
+            return await this.model.create(product);
         } catch (error) {
-            throw new Error(error);
+            throw error;
+        }
+    }
+    getAll = async () => {
+        try {
+            return await this.model.find({});
+        } catch (error) {
+            throw error;
+        }
+    }
+    getById = async (id) => {
+        try {
+            return await this.model.findById(id);
+        } catch (error) {
+            throw error;
+        }
+    }
+    delete = async (id) => {
+        try {
+            return await this.model.findByIdAndDelete(id);
+        } catch (error) {
+            throw error;
         }
     }
 }
-
-export const mongoDBManager = new MongoDBManager()
