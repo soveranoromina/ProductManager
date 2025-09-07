a3ecc8e627a4bf8c9e56fa043a4edc67b9894a5b
rominasoveranocode <rominasoverano.trabajo@gmail.com>
2025-09-07 15:44:24 -0300

    using params and filter

--- src/routes/ProductRouter.js
+++ src/routes/ProductRouter.js
@@ -4,8 +4,27 @@ const router = Router();
 
 router.get("/", async (req, res, next) => {
     try {
-        const products = await productManager.getProducts();
-        res.json(products);
+        const { page, limit, sort } = req.query;
+        const filter = req.body;
+        const response = await productManager.getProducts(page, limit, filter, sort);
+        const nextPage = response.hasNextPage
+            ? `http://localhost:8080/api/products?page=${response.nextPage}`
+            : null;
+        const prevPage = response.hasPrevPage
+            ? `http://localhost:8080/api/products?page=${response.prevPage}`
+            : null;
+        res.json({
+            payload: response.docs,
+            count: response.totalDocs,
+            totalPages: response.totalPages,
+            prevPage: response.prevPage,
+            nextPage: response.nextPage,
+            page: response.page,
+            hasPrevPage: response.hasPrevPage,
+            hasNextPage: response.hasNextPage,
+            nextLink: nextPage,
+            prevLink: prevPage,
+        });
     } catch (error) {
         next(error);
     }
@@ -14,14 +33,12 @@ router.get("/", async (req, res, next) => {
 router.post("/", async (req, res, next) => {
     try {
         const product = await productManager.addProduct(req.body);
-
         const io = req.app.get("socketServer");
         const { socketId } = req.body;
         if (socketId) {
             io.to(socketId).emit('newProduct', product);
         }
         io.emit('alertProduct', `Se ha añadido el producto ${product._id}`);
-
         res.json(product);
     } catch (error) {
         next(error);
@@ -40,10 +57,17 @@ router.get("/:id", async (req, res, next) => {
 
 router.put("/:id", async (req, res, next) => {
     try {
+        console.log(req.params, req.body)
         const { id } = req.params;
         const updatedProduct = req.body
         const product = await productManager.updateProduct(id, updatedProduct);
-        res.json(product);
+        const io = req.app.get("socketServer");
+        const { socketId } = req.body;
+        if (socketId) {
+            io.to(socketId).emit('updatedProduct', product);
+        }
+        io.emit('alertProduct', `Se ha actualizado el producto ${product._id}`);
+        res.json(product)
     } catch (error) {
         next(error);
     }
@@ -60,7 +84,6 @@ router.delete("/:id", async (req, res, next) => {
             io.to(socketId).emit('deletedProduct', 'Producto eliminado');
         }
         io.emit('alertProduct');
-
         res.json(product);
 
     } catch (error) {
