fe8498e8a32688da226c9a8f482a801abb3e21f5
rominasoveranocode <rominasoverano.trabajo@gmail.com>
2025-09-08 18:12:35 -0300

    new function "add to cart"

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -2,6 +2,7 @@ const socket = io();
 const prods = document.getElementById('prods');
 const pagination = document.getElementById('pagination');
 let currentUrl = "/api/products"
+let cartID = ""
 
 function createProductDescription(product) {
   const p = document.createElement("p");
@@ -17,18 +18,56 @@ function createProductTitle(product) {
   return h;
 }
 
-function createCartBTN() {
-  const h = document.createElement("button");
-  h.textContent = `add to cart`;
-  h.style.whiteSpace = "inline";
-  return h;
+function addToCartBTN(pid) {
+  const btn = document.createElement("button");
+  btn.id = pid;
+  btn.textContent = "add to cart";
+  btn.style.whiteSpace = "inline";
+
+  btn.addEventListener("click", async () => {
+    if (cartID === "") {
+      cartID = await createCart();
+      console.log("Carrito creado:", cartID);
+    }
+    await addProductToCart(pid);
+  });
+
+  return btn;
+}
+
+async function createCart() {
+  try {
+    const response = await fetch(`/api/carts/`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" }
+    });
+
+    const data = await response.json();
+    return data._id;
+  } catch (err) {
+    console.error("Error en el CREATE:", err);
+  }
+}
+
+async function addProductToCart(pid) {
+  try {
+    const response = await fetch(`/api/carts/${cartID}/product/${pid}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" }
+    });
+
+    const data = await response.json();
+    console.log("Producto agregado:", data);
+  } catch (err) {
+    console.error("Error en el UPDATE:", err);
+  }
 }
 
 function createProductDiv(product) {
   const div = document.createElement("div");
   div.appendChild(createProductTitle(product))
   div.appendChild(createProductDescription(product));
-  div.appendChild(createCartBTN());
+  div.appendChild(addToCartBTN(product._id));
   return div;
 }
 
@@ -49,17 +88,17 @@ async function getProducts(url = currentUrl) {
       prods.appendChild(div);
     });
 
-    pagination.innerHTML = ""; 
+    pagination.innerHTML = "";
     if (products.nextLink) {
       const nextBtn = document.createElement("button");
       nextBtn.textContent = "Pagina siguiente >";
-      nextBtn.onclick = () => getProducts(products.nextLink); 
+      nextBtn.onclick = () => getProducts(products.nextLink);
       pagination.appendChild(nextBtn);
     }
     if (products.prevLink) {
       const prevBtn = document.createElement("button");
       prevBtn.textContent = "< Pagina anterior";
-      prevBtn.onclick = () => getProducts(products.prevLink); 
+      prevBtn.onclick = () => getProducts(products.prevLink);
       pagination.appendChild(prevBtn);
     }
 
